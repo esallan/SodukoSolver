a48e0fda6d48f6710db0b8065e7afb8fe6ed88f7
Erika Sällberg <erikasallberg@hotmail.com>
2024-12-06 11:32:15 +0100

    test

---
+++ SodukoSolver/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SodukoSolver {
+	requires org.junit.jupiter.api;
+	requires junit;
+	requires java.desktop;
+}
---
+++ SodukoSolver/src/sodukoSolver/SodukoSolverMain.java
@@ -0,0 +1,10 @@
+package sodukoSolver;
+
+public class SodukoSolverMain {
+
+	public static void main(String[] args) {
+		SudokuSolver soduko = new SudokuSolver();
+		new SodukoGUI().createWindow();
+	}
+
+}
